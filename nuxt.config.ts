4ae88f777ebee9282ca0d341d06967cbda8bdeb7
Andrea Minato <andreaminato@outlook.com>
2024-11-11 17:54:04 +0100

    prepare to nuxt4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@vueuse/nuxt', '@pinia/nuxt'],
